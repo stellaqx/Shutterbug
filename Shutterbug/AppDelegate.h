4871ef85c25a44f15896710a5c8fdb67354a2680
Qian <ee.xuqian@gmail.com>
2020-04-22 23:04:01 -0700

    Initial Commit

---
+++ Shutterbug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Shutterbug
+//
+//  Created by Qian on 4/22/20.
+//  Copyright © 2020 Stella Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Shutterbug/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Shutterbug
+//
+//  Created by Qian on 4/22/20.
+//  Copyright © 2020 Stella Xu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
